b33237b0665fcc10f751650d70ce641a55251621
Workwithnk <workofnk@gmail.com>
2021-03-09 17:06:49 +0530

    corrections on hamburgerMenu

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log("this is nk");
+const togglerMenu = document.querySelector('.togglerMenu');
+
+const toggleMenu = document.querySelector('.toggleMenu');
+toggleMenu.addEventListener('click',()=>{
+    if(togglerMenu.style.display === 'flex'){
+        togglerMenu.style.display = 'none'
+    }
+    else{
+        togglerMenu.style.display = 'flex'
+    }
+});
+togglerMenu.addEventListener('click',()=>{
+    togglerMenu.style.display = 'none';
+});
+
+const togglerLi = document.querySelectorAll('.togglerMenu ul li');
+togglerLi.addEventListener('click',()=>{
+    togglerMenu.style.display = 'none';
+});
